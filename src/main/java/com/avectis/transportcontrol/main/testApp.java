cd70e35840fa278dc56c8952d8ae193c3895b290
DPoplauski <DPoplauski@POPLAVSKY2.avectis.local>
2016-08-18 09:40:04 -0300

    test class

---
+++ src/main/java/com/avectis/transportcontrol/main/testApp.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avectis.transportcontrol.main;
+import com.avectis.transportcontrol.entity.*;
+/**
+ *
+ * @author DPoplauski
+ */
+public class testApp {
+
+    public static void main(String args[]) {
+        Driver dr1 = new Driver();
+        dr1.setName("hitler");
+        System.out.println(dr1.getName());
+    }
+    
+}
