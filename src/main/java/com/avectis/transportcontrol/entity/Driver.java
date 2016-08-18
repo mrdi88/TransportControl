97062e55f6159d3781206f85e3c0775feaea0f3c
DPoplauski <DPoplauski@POPLAVSKY2.avectis.local>
2016-08-18 08:22:24 -0300

    we

---
+++ src/main/java/com/avectis/transportcontrol/entity/Driver.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avectis.transportcontrol.entity;
+
+/**
+ *
+ * @author DPoplauski
+ */
+public class Driver {
+
+    private long driverId;
+    private String name;
+    private String mobileNumber;
+    private String organization;
+
+    public String getOrganization() {
+        return organization;
+    }
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public long getDriverId() {
+        return driverId;
+    }
+    public void setDriverId(long driverId) {
+        this.driverId = driverId;
+    }
+    public Driver() {
+    }
+}
