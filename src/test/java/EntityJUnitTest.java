2159a63bc62b9b03b40221e280faf9ebefabed13
DPoplauski <DPoplauski@POPLAVSKY2.avectis.local>
2016-08-22 11:41:43 -0300

    наконец заработало 3 master

--- src/test/java/EntityJUnitTest.java
+++ src/test/java/EntityJUnitTest.java
@@ -209,7 +209,7 @@ public class EntityJUnitTest {
     @Test
     public void testCard(){
         try {
-
+             System.out.println("End");       
         }
         catch(Exception e) {
             System.out.println("ex: " + e);
