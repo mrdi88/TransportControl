da8d0dc4d7d0b92906be09c37bf902a9d2085c6f
ablovatsky <vitaly.ablovatsky@gmail.com>
2016-08-30 15:30:34 +0300

    Add packge "Control"

---
+++ src/main/java/com/avectis/transportcontrol/control/barrier/Barrier.java
@@ -0,0 +1,39 @@
+package com.avectis.transportcontrol.control.barrier;
+
+/**
+ * Created by vitaly on 24.08.2016.
+ */
+public class Barrier {
+
+    private String   name;
+    private boolean  state;
+    private BarrierAdapter barrierAdapter;
+
+    public Barrier(String barrierName, BarrierAdapter barrierAdapter){
+        this.name = barrierName;
+        this.state = false;
+        this.barrierAdapter = barrierAdapter;
+    }
+
+    public void Open(){
+        this.barrierAdapter.Open();
+    }
+    public void Close(){
+        this.barrierAdapter.Close();
+    }
+    public void GetState(){
+        this.barrierAdapter.GetState();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
